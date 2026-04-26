4e80af1e916038034521e7962540a8333082aef1
NEKU2536 <6kkxs6nxhs@privaterelay.appleid.com>
2026-04-26 23:53:46 +0900

    Create puzzles.js

---
+++ puzzles.js
@@ -0,0 +1,32 @@
+const puzzles = {
+
+"8-1":{
+size:8,
+grid:[
+["ア"," ","イ","ウ"," ","エ","オ"," "],
+["カ","キ"," ","ク","ケ"," ","コ","サ"],
+[" ","シ","ス"," ","セ","ソ"," ","タ"],
+["チ"," ","ツ","テ"," ","ト","ナ"," "],
+["ニ","ヌ"," ","ネ","ノ"," ","ハ","ヒ"],
+[" ","フ","ヘ"," ","ホ","マ"," ","ミ"],
+["ム"," ","メ","モ"," ","ヤ","ユ"," "],
+["ヨ","ラ"," ","リ","ル"," ","レ","ロ"]
+]
+},
+
+"8-2":{size:8,grid:Array(8).fill().map(()=>Array(8).fill(" "))},
+"8-3":{size:8,grid:Array(8).fill().map(()=>Array(8).fill(" "))},
+"8-4":{size:8,grid:Array(8).fill().map(()=>Array(8).fill(" "))},
+"8-5":{size:8,grid:Array(8).fill().map(()=>Array(8).fill(" "))},
+
+"10-1":{
+size:10,
+grid:Array(10).fill().map(()=>Array(10).fill(" "))
+},
+
+"10-2":{size:10,grid:Array(10).fill().map(()=>Array(10).fill(" "))},
+"10-3":{size:10,grid:Array(10).fill().map(()=>Array(10).fill(" "))},
+"10-4":{size:10,grid:Array(10).fill().map(()=>Array(10).fill(" "))},
+"10-5":{size:10,grid:Array(10).fill().map(()=>Array(10).fill(" "))}
+
+};
